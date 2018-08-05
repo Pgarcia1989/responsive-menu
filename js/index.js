0b45e01c3bb4b9788511b4cb9acf90a7fb033709
Pgarcia1989 <pjgl19@gmail.com>
2018-08-05 21:26:19 +0200

    añadido interaccion menu

---
+++ js/index.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  
+    $( "#responsive-menu-icon" ).click(function() {
+     
+    var nav = $(".nav-wrapper");
+    var navHeight = nav.height();     
+     if (navHeight < 150) {
+  TweenMax.to(nav, 1, {"height":280});
+      } else {
+        TweenMax.to(nav, 1, {"height":50});
+      };
+    });
+    
+    // menu icon jquery
+    
+    // when whole menu div is clicked
+    
+    $("#responsive-menu-icon").click( function() {
+      // if the menu height is not full, transform icon
+      var nav = $(".nav-wrapper");
+  var navHeight = nav.height();
+      
+      if (navHeight < 150) {
+        $("#a").addClass("rotate-down");
+      $("#responsive-menu-icon").addClass("menu-up");
+       $("#b").addClass("disappear");
+     
+       $("#c").addClass("rotate-up");
+        
+        
+      } else {
+        
+        // if it's not less than full height, remove animation classes
+        $("#a").removeClass("rotate-down");
+      $("#responsive-menu-icon").removeClass("menu-up");
+       $("#b").removeClass("disappear");
+     
+       $("#c").removeClass("rotate-up");
+    
+      
+      };
+      
+      
+      
+    });
+    
+    
+    
+    
+  });
